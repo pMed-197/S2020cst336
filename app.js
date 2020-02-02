9945b7d97c6c291ceac66f180170a56f669e9b47
EC2 Default User <ec2-user@ip-172-31-86-47.ec2.internal>
2020-02-02 05:42:27 +0000

    5th Commit

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var portfolioRouter = require('./routes/portfolio');
 var usersRouter = require('./routes/users');
-
+var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/porfolio', indexRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
